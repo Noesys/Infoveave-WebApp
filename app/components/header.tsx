cb2b82e784a9173d5ae62a7a7249795acf69d9b2
Naresh Jois <naresh@noesyssoftware.com>
2016-08-08 15:18:18 +0530

    Fix: Permissions in Header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,6 +1,6 @@
 ﻿/**
  * Copyright © 2015-2016 Noesys Software Pvt.Ltd. - All Rights Reserved
- * -------------
+ * -------------',
  * This file is part of Infoveave.
  * Infoveave is dual licensed under Infoveave Commercial License and AGPL v3
  * -------------
@@ -37,8 +37,9 @@ import injectTapEventPlugin from "react-tap-event-plugin";
 import FontIcon from "material-ui/lib/font-icon";
 import Dialog from "material-ui/lib/dialog";
 import { Urls } from "../data/constants";
+import { CanAccess } from "./canAccess";
 /* tslint:disable */
-let versionInfo: any =  require("./../version.json");
+let versionInfo: any = require("./../version.json");
 /* tslint:enable */
 injectTapEventPlugin();
 
@@ -51,8 +52,9 @@ class InfoboardMenuLink {
         this.id = id;
         this.name = name;
         this.shortCode = shortCode;
-        if (name.split(" ").length > 1) {
-            this.side = name.split(" ")[0][0].toUpperCase() + name.split(" ")[1][0].toLowerCase();
+        if (name.trim().split(" ").length > 1) {
+            let s = _.filter(name.split(" "), (n) => n !== "");
+            this.side = s[0][0].toUpperCase() + s[1][0].toLowerCase();
         } else {
             this.side = _s.titleize(name.substr(0, 2));
         }
@@ -85,14 +87,14 @@ let ShouldRender = (props: { _store: AuthorisationStore, view: string, action?: 
     let permissions = props._store.GetUserPermissions();
     let fperms = _.filter(permissions, (a) => a.view === props.view);
     if (props.action != null) {
-        fperms =  _.filter(permissions, (a) => a.action === props.action);
+        fperms = _.filter(fperms, (a) => a.action === props.action);
     }
-    return (fperms.length > 0) ? props.children : <div/>;
+    return (fperms.length > 0) ? props.children : <li/>;
 };
 
 let ShouldRenderIncludes = (props: { _store: AuthorisationStore, views: string[], children?: any }) => {
     let permissions = props._store.GetUserPermissions();
-    return (_.find(permissions, (a) => _.includes(props.views, a.view)) != null) ? props.children : <div/>;
+    return (_.find(permissions, (a) => _.includes(props.views, a.view)) != null) ? props.children : <li/>;
 };
 
 export class Header extends BaseComponent<IHeaderProperties, IHeaderState> {
@@ -105,7 +107,7 @@ export class Header extends BaseComponent<IHeaderProperties, IHeaderState> {
         this.state = {
             helpOpen: false,
             user: { name: "Infoveave User", imagePath: "", username: "", id: null, version: null },
-            currentHelpSteps : [],
+            currentHelpSteps: [],
             currentHelpStep: -1,
             currentHelpName: null
         };
@@ -120,7 +122,7 @@ export class Header extends BaseComponent<IHeaderProperties, IHeaderState> {
     };
 
     updateUserInfo = (forceUpdate: boolean) => {
-          this._dataStore.GetUserInfo(forceUpdate).then((result) => {
+        this._dataStore.GetUserInfo(forceUpdate).then((result) => {
             let user = _.assign(_.clone(this.state.user), result) as any;
             user.name = result.firstName + " " + result.lastName;
             this.setState({ user: user } as any);
@@ -172,14 +174,14 @@ export class Header extends BaseComponent<IHeaderProperties, IHeaderState> {
             return (
                 <li key={"lmil" + ix} className="">
                     <Link to={"/app/infoboards/" + i.id}>{i.name}</Link>
-                    </li>
+                </li>
             );
         });
     };
 
     saveInfoboardClick = () => {
         if (_s.isBlank(this.state.newInfoboardName)) return;
-         this.setState({addInfoboardDisabled: true});
+        this.setState({ addInfoboardDisabled: true });
         (this.refs["modalAdd"] as ModalDialog).hideDialog();
         this._dataStore.AddInfoboard({ name: this.state.newInfoboardName }).then((results) => {
             let boards = this.state.infoboards;
@@ -190,11 +192,11 @@ export class Header extends BaseComponent<IHeaderProperties, IHeaderState> {
     };
 
     newInfoboardNameChange = (newValue: string) => {
-        this.updateStateImmutable({ newInfoboardName:  newValue});
+        this.updateStateImmutable({ newInfoboardName: newValue });
     };
 
-        addInfoboard = () => {
-        this.setState({addInfoboardDisabled: false});
+    addInfoboard = () => {
+        this.setState({ addInfoboardDisabled: false });
         (this.refs["modalAdd"] as ModalDialog).showDialog();
         return;
     };
@@ -204,83 +206,99 @@ export class Header extends BaseComponent<IHeaderProperties, IHeaderState> {
     };
 
     reload = () => {
-       window.location.reload(true);
+        window.location.reload(true);
     };
 
     render() {
         let userImage = (this.state.user != null && this.state.user.id != null) ? `${Urls.baseUrl()}/User/${Urls.getTenant()}/${this.state.user.id}/ProfileImage?q=${this._makeid(5)}` : "";
         return (
             <nav className="navbar navbar-default navbar-fixed-top">
-            <ModalDialog title={this.gs("AddInfoboardTitle")} size={ModalDialogSize.Medium} ref="modalAdd" actions={
-                <Button caption={this.gs("Save") } type="button" disabled={this.state.addInfoboardDisabled} title="Save" onClick={this.saveInfoboardClick} />
-            }><br/>
-                <ModalContent>
-                    <Form ref="form">
-                        <TextBox type="text" caption={this.gs("Name")} value={this.state.newInfoboardName} onChange={this.newInfoboardNameChange}/>
-                    </Form>
-                </ModalContent>
-            </ModalDialog>
-            <div className="container-fluid">
-                <div className="navbar-header">
-                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span className="sr-only">Toggle navigation</span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                </button>
-                <a className="navbar-brand" href="#"><InfoveaveLogo/></a>
-                </div>
-
-                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul className="nav navbar-nav">
-                </ul>
-                <ul className="nav navbar-nav navbar-right">
-                    <li className="dropdown">
-                        <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i className="fa fa-book" aria-hidden="true"></i>
-                            <span className="title"> {this.gs("Infoboards") }</span>
-                        </a>
-                        <ul className="dropdown-menu">
-                            { this.boardLinksDom() }
-                            <li role="separator" className="divider"></li>
-                                <li className="">
-                                    <a onClick={this.addInfoboard} data-help="leftSidebar.AddInfoboard" style={{ "cursor": "pointer" }}>{this.gs("AddInfoboard") } <i className="fa fa-plus-circle" aria-hidden="true"></i></a>
-                                </li>
-                        </ul>
-                    </li>
-                    <li className="dropdown">
-                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i className="fa fa-list-alt" aria-hidden="true"></i> Reports</a>
-                        <ul className="dropdown-menu">
-                            <li><Link to="/app/reportManager">{this.gs("Reports") }</Link></li>
-                            <li><Link to="/app/queryBuilder">{this.gs("QueryBulider") }</Link></li>
-                        </ul>
-                    </li>
-                    <li className="dropdown">
-                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i className="fa fa-user" aria-hidden="true"></i> Administration</a>
-                        <ul className="dropdown-menu">
-                            <li><Link to="/app/roleManager">Roles</Link></li>
-                            <li><Link to="/app/userManager">Manage Users</Link></li>
-                            <li><Link to="/app/dataSourceManager">Data Sources</Link></li>
-                            <li><Link to="/app/colorPalette">Color Palette</Link></li>
-                        </ul>
-                    </li>
-                    <li><div className="pull-right hidden-sm hidden-xs">
-                    <div className="header-inner">
-                        <IconMenu desktop={true} iconButtonElement={<IconButton><div className="thumbnail-wrapper d32 circular inline hint--left" style={{ fontSize: "12px" }}>
-                            <img src={userImage} className="hint--left" data-hint="Profile, Options"  alt={this.gs("EditProfile") } data-src={userImage} data-src-retina={userImage} width="32" height="32"/></div></IconButton>}
-                            anchorOrigin={{ horizontal: "right", vertical: "top" }} targetOrigin={{ horizontal: "right", vertical: "top" }} >
-                            <MenuItem primaryText={this.state.user.name} style={{ fontSize: "14px" }} onTouchTap={this.reload} />
-                            <Divider/>
-                            <MenuItem primaryText={<Link to="/app/profile" style={{ color: "black" }}>{this.gs("EditProfile") }</Link>}  />
-                            <MenuItem primaryText={this.gs("LogOut") }  onTouchTap={this.logoutClick} />
-                            <Divider/>
-                            <MenuItem primaryText={`${this.gs("Version")} : A.${this.state.user.version} C.${versionInfo.version}`}/>
-                        </IconMenu>
-                        <i className="p-l-5 demo-fs-23 fa fa-question-circle"/>
+                <ModalDialog title={this.gs("AddInfoboardTitle") } size={ModalDialogSize.Medium} ref="modalAdd" actions={
+                    <Button caption={this.gs("Save") } type="button" disabled={this.state.addInfoboardDisabled} title="Save" onClick={this.saveInfoboardClick} />
+                }><br/>
+                    <ModalContent>
+                        <Form ref="form">
+                            <TextBox type="text" caption={this.gs("Name") } value={this.state.newInfoboardName} onChange={this.newInfoboardNameChange}/>
+                        </Form>
+                    </ModalContent>
+                </ModalDialog>
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand" href="#"><InfoveaveLogo/></a>
                     </div>
-                </div></li>
-                </ul>
+
+                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul className="nav navbar-nav">
+                        </ul>
+                        <ul className="nav navbar-nav navbar-right">
+                            <ShouldRender _store={this._authStore} view="Infoboards">
+                                <li className="dropdown">
+                                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i className="fa fa-book" aria-hidden="true"></i>
+                                        <span className="title"> {this.gs("Infoboards") }</span>
+                                    </a>
+                                    <ul className="dropdown-menu">
+                                        { this.boardLinksDom() }
+                                        <li role="separator" className="divider"></li>
+                                        <ShouldRender _store={this._authStore} view="Infoboards" action="Add">
+                                            <li className="">
+                                                <a onClick={this.addInfoboard} data-help="leftSidebar.AddInfoboard" style={{ "cursor": "pointer" }}>{this.gs("AddInfoboard") } <i className="fa fa-plus-circle" aria-hidden="true"></i></a>
+                                            </li>
+                                        </ShouldRender>
+                                    </ul>
+                                </li>
+                            </ShouldRender>
+                            <ShouldRender _store={this._authStore} view="QueryBuilder">
+                                <li className="dropdown">
+                                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i className="fa fa-list-alt" aria-hidden="true"></i> Reports</a>
+                                    <ul className="dropdown-menu">
+                                        <li><Link to="/app/reportManager">{this.gs("Reports") }</Link></li>
+                                        <li><Link to="/app/queryBuilder">{this.gs("QueryBulider") }</Link></li>
+                                    </ul>
+                                </li>
+                            </ShouldRender>
+                            <ShouldRenderIncludes _store={this._authStore} views={["DataSource", "User", "Role", "ColorPalette"]}>
+                                <li className="dropdown">
+                                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i className="fa fa-user" aria-hidden="true"></i> Administration</a>
+                                    <ul className="dropdown-menu">
+                                        <ShouldRender _store={this._authStore} view="Role">
+                                            <li><Link to="/app/roleManager">Roles</Link></li>
+                                        </ShouldRender>
+                                        <ShouldRender _store={this._authStore} view="User">
+                                            <li><Link to="/app/userManager">Manage Users</Link></li>
+                                        </ShouldRender>
+                                        <ShouldRender _store={this._authStore} view="DataSource">
+                                            <li><Link to="/app/dataSourceManager">Data Sources</Link></li>
+                                        </ShouldRender>
+                                        <ShouldRender _store={this._authStore} view="ColorPalette">
+                                            <li><Link to="/app/colorPalette">Color Palette</Link></li>
+                                        </ShouldRender>
+                                    </ul>
+                                </li>
+                            </ShouldRenderIncludes>
+                            <li><div className="pull-right hidden-sm hidden-xs">
+                                <div className="header-inner">
+                                    <IconMenu desktop={true} iconButtonElement={<IconButton><div className="thumbnail-wrapper d32 circular inline hint--left" style={{ fontSize: "12px" }}>
+                                        <img src={userImage} className="hint--left" data-hint="Profile, Options"  alt={this.gs("EditProfile") } data-src={userImage} data-src-retina={userImage} width="32" height="32"/></div></IconButton>}
+                                        anchorOrigin={{ horizontal: "right", vertical: "top" }} targetOrigin={{ horizontal: "right", vertical: "top" }} >
+                                        <MenuItem primaryText={this.state.user.name} style={{ fontSize: "14px" }} onTouchTap={this.reload} />
+                                        <Divider/>
+                                        <MenuItem primaryText={<Link to="/app/profile" style={{ color: "black" }}>{this.gs("EditProfile") }</Link>}  />
+                                        <MenuItem primaryText={this.gs("LogOut") }  onTouchTap={this.logoutClick} />
+                                        <Divider/>
+                                        <MenuItem primaryText={`${this.gs("Version")} : A.${this.state.user.version} C.${versionInfo.version}`}/>
+                                    </IconMenu>
+                                    <i className="p-l-5 demo-fs-23 fa fa-question-circle"/>
+                                </div>
+                            </div></li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
             </nav>
         );
     }
